e85275efc2e3286deada0322ec9013de35816791
Pablo Souza Grigoletti <pablo.grigoletti@serpro.gov.br>
2024-01-12 22:04:17 -0300

    Atualizando endpoints para usar o banco de dados real

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,12 @@
+from fast_zero.schemas import UserPublic
+
+
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
 def test_root_deve_retornar_200_e_ola_mundo(client):
     response = client.get('/')
 
@@ -5,38 +14,13 @@ def test_root_deve_retornar_200_e_ola_mundo(client):
     assert response.json() == {'message': 'Olá Mundo!'}
 
 
-def test_create_user(client):
-    response = client.post(
-        '/users/',
-        json={
-            'username': 'alice',
-            'email': 'alice@example.com',
-            'password': 'secret',
-        },
-    )
-    assert response.status_code == 201
-    assert response.json() == {
-        'username': 'alice',
-        'email': 'alice@example.com',
-        'id': 1,
-    }
-
-
 def test_read_users(client):
-    response = client.get('/users/')
+    response = client.get('/users')
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -53,8 +37,24 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
-
     assert response.status_code == 200
     assert response.json() == {'detail': 'User deleted'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == 201
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
